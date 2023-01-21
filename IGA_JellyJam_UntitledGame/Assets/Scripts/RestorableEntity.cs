7c9dec127672597bb312c908363b4ba59b313da1
John Peter McGrath <mrjbrpg@gmail.com>
2023-01-21 12:54:51 -0800

    Added Test object to restore
    Added a small object to test the restorable object trigger collision and defined ground ad game over touch.

---
+++ IGA_JellyJam_UntitledGame/Assets/Scripts/RestorableEntity.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestorableEntity : MonoBehaviour
+{
+    [SerializeField] private Material materialDecay;
+    [SerializeField] private Material materialRestore;
+
+    public void TurnFromDecayToRestore()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().material = materialRestore;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
